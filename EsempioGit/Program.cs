f87445fc9bdbc1ce7fd85ce0496c8b194bd20b9b
labinfo <labinfo4@secondarieclago.it>
2019-08-31 19:17:20 +0200

    Creazione soluztion vs2019

---
+++ EsempioGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EsempioGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
